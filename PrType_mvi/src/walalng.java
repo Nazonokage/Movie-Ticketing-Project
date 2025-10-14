71b71e47fb8c19196e2a04d00029b119186a9b32
Nazonokage <jova.porras.ui@phinmaed.com>
2025-10-14 19:52:48 +0800

    COPY

---
+++ PrType_mvi/src/walalng.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class walalng {
+    public static void main(String[] args) {
+
+        Scanner npt = new Scanner(System.in);
+
+        System.out.println("input something");
+        String input = npt.nextLine();
+
+        System.out.println(input);
+    }
+}
